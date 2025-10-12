4b306410e27194ce75e9c475b8cfa850981deed4
Abhishek Sharma <156454751+LoneLeveling@users.noreply.github.com>
2025-10-13 03:41:11 +0530

    Added OCP example 3

---
+++ src/Lesson2_OpenClosedPrinciple/Example3/ProblematicCode/ReportGenerator.java
@@ -0,0 +1,15 @@
+package Lesson2_OpenClosedPrinciple.Example3.ProblematicCode;
+
+public class ReportGenerator {
+    public String generateReport(String reportType)
+    {
+        if (reportType.equals("PDF")){
+            return "Generating PDF report...";
+    }
+        else if (reportType.equals("Word"))
+        {
+        return "Generating Word Doc report...";
+        }
+        return "Report type not supported.";
+    }
+}
