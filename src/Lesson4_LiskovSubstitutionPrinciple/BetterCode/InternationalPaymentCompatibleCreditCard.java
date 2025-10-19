14bcc8e89415ff08f4a297652701369652fc6767
Abhishek Sharma <abhioptcs2000@gmail.com>
2025-10-19 23:34:26 +0530

    Added comments to Int. Payment Service Interface

--- src/Lesson4_LiskovSubstitutionPrinciple/BetterCode/InternationalPaymentCompatibleCreditCard.java
+++ src/Lesson4_LiskovSubstitutionPrinciple/BetterCode/InternationalPaymentCompatibleCreditCard.java
@@ -1,5 +1,7 @@
 package Lesson4_LiskovSubstitutionPrinciple.BetterCode;
 
+
+//Now all classes wanting to enable InternationalPayment service will implement this interface.
 public interface InternationalPaymentCompatibleCreditCard
 {
 
